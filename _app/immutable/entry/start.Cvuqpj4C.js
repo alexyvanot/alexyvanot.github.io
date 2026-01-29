546bb64bc01345c5658ce3d655c87ba32df87504
alexyvanot <48055002+alexyvanot@users.noreply.github.com>
2026-01-29 11:28:56 +0000

    Deploying to gh-pages from @ alexyvanot/alexyvanot.github.io@080165e1abe6bb8e7a704759c933cf42196c4759 🚀

--- _app/immutable/entry/start.Cvuqpj4C.js
+++
@@ -1,1 +0,0 @@
-import{b as s}from"../chunks/CkOrSXI5.js";import{w as t}from"../chunks/Mess9OX4.js";export{t as load_css,s as start};
---
+++ _app/immutable/entry/start.b8jogiS1.js
@@ -0,0 +1,1 @@
+import{b as s}from"../chunks/MAPz7wYf.js";import{w as t}from"../chunks/Bu0lfiIf.js";export{t as load_css,s as start};
